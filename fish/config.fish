2389d33cd1d77e5e1b89503805782fac14f16531
Daniel Duan <daniel@duan.ca>
2019-03-08 12:59:38 -0800

    vim goodness

--- fish/config.fish
+++ fish/config.fish
@@ -13,9 +13,10 @@ end
 
 set fish_key_bindings fish_user_key_bindings
 
-export FZF_DEFAULT_COMMAND='/usr/local/bin/rg --files -S --no-ignore --hidden --follow --glob "!tags" --glob "!.git/*" --glob "!build/" --glob "!.build/" --glob "!.DS_Store" --glob "!build.noindex" --glob "!Pods" --glob "!deps"'
+export FZF_DEFAULT_COMMAND='/usr/local/bin/rg --files -S --no-ignore --hidden --follow --glob "!tags" --glob "!.git/*" --glob "!build/" --glob "!.build/" --glob "!.DS_Store" --glob "!build.noindex" --glob "!Pods" --glob "!deps" --glob "!bazel-ios"'
 export FZF_DEFAULT_OPTS="--height 37.5% --reverse --preview 'file {}' --preview-window down:1"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
 
 if status --is-login
     set -x PATH $PATH ~/bin
