b4b840f4e4b07d30f8402ad9ddcc9bb6781cd0af
Daniel Duan <daniel@duan.ca>
2022-02-25 20:38:19 -0800

    Fix fish init point

--- nixpkgs/fish.nix
+++ nixpkgs/fish.nix
@@ -165,7 +165,7 @@
     fish_mode_prompt = "";
   };
 
-  interactiveShellInit = ''
+  shellInit = ''
     # nix
     if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
         fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
