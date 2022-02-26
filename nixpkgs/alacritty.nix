604f2e5af7461498e26f1a26c26de9a9a77b807b
Daniel Duan <daniel@duan.ca>
2022-02-25 21:01:57 -0800

    Add Alacritty style

--- nixpkgs/alacritty.nix
+++ nixpkgs/alacritty.nix
@@ -42,5 +42,42 @@
       animation = "EaseOutExpo";
       duration = 1;
     };
+    cursor = {
+      style.blinking = "Always";
+      blink_interval = 500;
+    };
+    colors = {
+      # Default colors
+      primary = {
+        background = "0x212121";
+        foreground = "0xdcdccc";
+      };
+
+      # Colors the cursor will use if `custom_cursor_colors` is true
+
+      # Normal colors
+      normal = {
+        black = "0x4d4d4d"; # 0
+        red = "0xfb2573"; # 1
+        green = "0x98e123"; # 2
+        yellow = "0xeedfb5"; # 3
+        blue = "0x45a9d0"; # 4
+        magenta = "0xd190c0"; # 5
+        cyan = "0x1070b0"; # 6
+        white = "0xdcdbcd"; # 7
+      };
+
+      # Bright colors
+      bright = {
+        black = "0x768f81"; # 8
+        red = "0x999999"; # 9
+        green = "0xc2bfa2"; #10
+        yellow = "0xddcfa4"; #11
+        blue = "0x9dbdee"; #12
+        magenta = "0xe097d0"; #13
+        cyan = "0xa4dee2"; #14
+        white = "0xffffff"; #15
+      };
+    };
   };
 }
