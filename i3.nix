735d64e4b8316ae8b27b48e38b14f97c3d9f1a37
Daniel Duan <daniel@duan.ca>
2022-03-10 05:30:12 +0000

    Add a little i3

--- i3.nix
+++ i3.nix
@@ -1,5 +1,18 @@
-{}:
+{ alacritty }:
 let mod = "Mod1"; in
 {
   enable = true;
+  config = {
+    modifier = mod;
+    window = {
+      border = 0;
+      titlebar = false;
+    };
+    terminal = "${alacritty}/bin/alacritty";
+    defaultWorkspace = "workspace number 9";
+    fonts = {
+      names = [ "SF Mono" "pango:DejaVu Sans Mono" ];
+      size = 10.0;
+    };
+  };
 }
