401bc3e8d6087e3c84fec83762fd89dff18be069
Daniel Duan <daniel@duan.ca>
2022-03-11 07:00:23 +0000

    fixup! Put constants in lib

--- config/programs/linux.nix
+++ config/programs/linux.nix
@@ -1,7 +1,6 @@
 { pkgs }:
-let callPackages = (import ../../lib).callPackages {
-  pkgs; }; in
-  import ./common.nix { inherit pkgs; } // {
+let callPackages = (import ../../lib).callPackages pkgs; in
+import ./common.nix { inherit pkgs; } // {
   tmux = callPackages ../tmux/linux.nix { };
   i3status = callPackages ../i3status.nix { };
   rofi = callPackages ../rofi.nix { };
