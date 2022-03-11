9afc73532b6294f5842480459d8715a66e12d797
Daniel Duan <daniel@duan.ca>
2022-03-10 23:01:02 -0800

    Put constants in lib (#3)

--- config/programs/linux.nix
+++ config/programs/linux.nix
@@ -1,12 +1,5 @@
 { pkgs }:
-let
-  localPackages = import ../../pkgs {
-    inherit pkgs;
-  };
-  callPackages = path: overrides:
-    let f = import path;
-    in f ((builtins.intersectAttrs (builtins.functionArgs f) (pkgs // localPackages)) // overrides);
-in
+let callPackages = (import ../../lib).callPackages pkgs; in
 import ./common.nix { inherit pkgs; } // {
   tmux = callPackages ../tmux/linux.nix { };
   i3status = callPackages ../i3status.nix { };
--- lib/default.nix
+++ lib/default.nix
@@ -1,11 +1,11 @@
-{ pkgs }:
-let
-  localPackages = import ../pkgs {
-    inherit pkgs;
-  };
-in
+let keyValueAttrs = values: builtins.listToAttrs (map (v: { name = v; value = v; }) values); in
 {
-  callPackages = path: overrides:
-    let f = import path;
-    in f ((builtins.intersectAttrs (builtins.functionArgs f) (pkgs // localPackages)) // overrides);
+  callPackages = pkgs: path: overrides:
+    let
+      localPackages = import ../pkgs { inherit pkgs; };
+      f = import path;
+    in
+    f ((builtins.intersectAttrs (builtins.functionArgs f) (pkgs // localPackages)) // overrides);
+  os = keyValueAttrs [ "darwin" "linux" ];
+  arch = keyValueAttrs [ "aarch64" "x86_64" ];
 }
