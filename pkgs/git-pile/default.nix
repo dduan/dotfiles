fbeed7b82e3da25ba703a4960476b82df18f2b9c
Daniel Duan <daniel@duan.ca>
2022-06-19 01:25:33 -0700

    Test git-pile PR

--- pkgs/git-pile/default.nix
+++ pkgs/git-pile/default.nix
@@ -1,16 +1,16 @@
 { stdenv, gh, fzy, fetchFromGitHub, ... }:
 stdenv.mkDerivation rec {
   pname = "git-pile";
-  version = "0.2.0";
+  version = "0.2.3.0";
   buildInputs = [
     gh
     fzy
   ];
   src = fetchFromGitHub {
-    owner = "keith";
+    owner = "dduan";
     repo = "git-pile";
-    rev = version;
-    sha256 = "sha256-M7A2xdUTrwc6xSEns2W4qTCMk6WnEN8mUtDBfHQNsH0=";
+    rev = "e66fcf94fb7f578565f05b8b80949c851ee7a663";
+    sha256 = "sha256-281haWgvqG72w/vYetAK7HjBZ27KHRG3R+pLriLkOco=";
     name = "git-pile-${version}-src";
   };
   patchPhase = ''
