81246ea5ffa0b56e9534f1faca7d7dfb5b54122c
Daniel Duan <daniel@duan.ca>
2023-02-03 14:15:54 -0800

    [Nixpkgs] Update to 22.11

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.url = "github:nix-community/home-manager/release-22.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     tre-command.url = "github:dduan/tre";
     ea.url = "github:dduan/ea";
   };
@@ -15,26 +15,34 @@
         in
         {
           name = "dan@${host}";
-          value = home-manager.lib.homeManagerConfiguration {
-            inherit system;
-            username = "dan";
-            configuration = if isDarwin then import ./config/home/darwin.nix else ./config/home/linux.nix;
-            homeDirectory = if isDarwin then "/Users/dan" else "/home/dan";
-            pkgs = import inputs.nixpkgs {
-              overlays = [
-                (final: prev:
-                  {
-                    tre-command = inputs.tre-command.defaultPackage.${system};
-                    ea = inputs.ea.defaultPackage.${system};
-                  })
+          value = home-manager.lib.homeManagerConfiguration
+            {
+              modules = [
+                (if isDarwin then ./config/home/darwin.nix else ./config/home/linux.nix)
+                {
+                  home = {
+                    username = "dan";
+                    homeDirectory = if isDarwin then "/Users/dan" else "/home/dan";
+                    stateVersion = "22.11";
+                  };
+                }
               ];
-              inherit system;
+              pkgs = import inputs.nixpkgs {
+                overlays = [
+                  (final: prev:
+                    {
+                      tre-command = inputs.tre-command.defaultPackage.${system};
+                      ea = inputs.ea.defaultPackage.${system};
+                    })
+                ];
+                inherit system;
+              };
             };
-          };
         };
     in
     with import ./lib; {
       homeConfigurations = builtins.listToAttrs [
+        (mkConfig { host = "mbp.local"; arch = arch.aarch64; os = os.darwin; })
         (mkConfig { host = "the-puter"; arch = arch.x86_64; os = os.linux; })
         (mkConfig { host = "dduan.local"; arch = arch.aarch64; os = os.darwin; })
         (mkConfig { host = "imac.local"; arch = arch.x86_64; os = os.darwin; })
