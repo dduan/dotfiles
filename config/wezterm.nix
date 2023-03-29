427700fd79d82175dd50e27deef8500c54705224
Daniel Duan <daniel@duan.ca>
2023-03-29 13:39:53 -0700

    [Wezterm] Adjust top/bottom padding

--- config/wezterm.nix
+++ config/wezterm.nix
@@ -16,7 +16,7 @@
       hide_tab_bar_if_only_one_tab = true,
       window_background_opacity = 0.85,
       macos_window_background_blur = 20,
-      window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+      window_padding = { left = 0, right = 0, },
       window_decoration = None,
     }
   '';
