6c8c464297c82a3e7b4f3404302e202d7fd1e98a
Daniel Duan <daniel@duan.ca>
2023-04-22 11:19:19 -0700

    [WezTerm] Remove opacity
    That sucked.

--- config/wezterm.nix
+++ config/wezterm.nix
@@ -14,7 +14,6 @@
       font = wezterm.font("SF Mono Nerd"),
       font_size = 14,
       hide_tab_bar_if_only_one_tab = true,
-      window_background_opacity = 0.9,
       macos_window_background_blur = 20,
       window_padding = { left = 0, right = 0, top = 0, },
       window_decoration = None,
