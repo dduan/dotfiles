40346eb089a9379a1f505da6bead5442208e80f7
Daniel Duan <daniel@duan.ca>
2023-04-30 15:52:14 -0700

    [NeoVim] Tweak LSP

--- config/neovim/lsp.lua
+++ config/neovim/lsp.lua
@@ -6,35 +6,32 @@ end
 
 local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end-- Set up nvim-cmp.
+end
+-- Set up nvim-cmp.
 local cmp = require'cmp'
 local lspkind = require('lspkind')
 cmp.setup({
     formatting = {
         format = lspkind.cmp_format({
-            mode = 'symbol_text', -- show only symbol annotations
+            mode = 'symbol', -- show only symbol annotations
             maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            ellipsis_char = '…', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
         })
     },
-    view = {
-        entries = {name = 'custom', selection_order = 'near_cursor' }
-    },
     snippet = {
-
         expand = function(args)
             vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
         end,
     },
     window = {
-        -- completion = cmp.config.window.bordered(),
+        -- completion = cmp.config.window(),
         -- documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-c>'] = cmp.mapping.abort(),
+        ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
