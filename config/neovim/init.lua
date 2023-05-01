2dd3a14582e30ad579bb1fce60b103cdd7d4b598
Daniel Duan <daniel@duan.ca>
2023-04-30 17:19:09 -0700

    [NeoVim] Set efm for Swift files

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -127,3 +127,30 @@ function _G.Toggle_venn()
         vim.b.venn_enabled = nil
     end
 end
+
+vim.api.nvim_create_autocmd(
+    {
+        "BufNewFile",
+        "BufRead",
+    },
+    {
+        pattern = { "*.swift" },
+        callback = function()
+            vim.api.nvim_buf_set_option(
+                vim.api.nvim_get_current_buf(),
+                "errorformat",
+                table.concat(
+                    {
+                        "%E%f:%l:%c: %trror: %m",
+                        "%W%f:%l:%c: %tarning: %m",
+                        "%I%f:%l:%c: note: %m",
+                        "%E%f:%l: %trror: %m",
+                        "%W%f:%l: %tarning: %m",
+                        "%I%f:%l: note: %m",
+                    },
+                    ","
+                )
+            )
+        end,
+    }
+)
