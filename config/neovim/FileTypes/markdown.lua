e03f3d43a1a7aea0c8d18c06042e7b0b98ec7600
Daniel Duan <daniel@duan.ca>
2023-05-15 13:11:51 -0700

    [Neovim] Disable LSP for Markdown

--- config/neovim/FileTypes/markdown.lua
+++ config/neovim/FileTypes/markdown.lua
@@ -1,2 +1,18 @@
 -- Markdown no folding
 vim.g.vim_markdown_folding_disabled = 1
+
+-- Disable LSP
+vim.api.nvim_create_autocmd(
+    {
+        "BufNewFile",
+        "BufRead",
+    },
+    {
+        pattern = { "*.md", "*.markdown" },
+        callback = function()
+            require("cmp").setup.buffer {
+                completion = { autocomplete = false }
+            }
+        end,
+    }
+)
