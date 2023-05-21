4b44a5546c6099c3670205ee2b69a86ae769215c
Daniel Duan <daniel@duan.ca>
2023-05-21 12:07:02 -0700

    [Rectangle] include config file

--- config/home/darwin.nix
+++ config/home/darwin.nix
@@ -1,5 +1,8 @@
 { config, pkgs, ... }:
 import ./common.nix // {
+  home.file = {
+    "Library/Application Support/Rectangle/RectangleConfig.json".source = ../RectangleConfig.json;
+  };
   home.packages = import ../packages/darwin.nix { inherit pkgs; };
   programs = import ../programs/darwin.nix { inherit pkgs; };
 }
