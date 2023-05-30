d4f9405449cf0ec257901e96cad71326ca671eec
Daniel Duan <daniel@duan.ca>
2023-05-29 23:27:15 -0700

    [Neovim] Stop showing LPS hints in lightline

--- config/neovim/Plugins/lightline.lua
+++ config/neovim/Plugins/lightline.lua
@@ -6,13 +6,6 @@ vim.g.lightline = {
             { 'readonly', 'relativepath', 'modified' }
         },
         right = {
-            {
-                'linter_errors',
-                'linter_warnings',
-                'linter_infos',
-                'linter_hints',
-                'linter_ok'
-            },
             { 'spell', 'fileencoding', 'percent', 'column' },
         }
     },
@@ -23,28 +16,8 @@ vim.g.lightline = {
     component_function = {
         gitbranch = 'FugitiveHead',
     },
-    component_expand = {
-        linter_hint = 'lightline#lsp#hints',
-        linter_infos = 'lightline#lsp#infos',
-        linter_warnings = 'lightline#lsp#warnings',
-        linter_errors = 'lightline#lsp#errors',
-        linter_ok = 'lightline#lsp#ok',
-    },
-    component_type = {
-        linter_hints = 'right',
-        linter_infos = 'right',
-        linter_warnings = 'warning',
-        linter_errors = 'error',
-        linter_ok = 'right',
-    },
     subseparator = {
         left = '',
         right = ''
     }
 }
-
-vim.g["lightline#lsp#indicator_hints"] = "\u{f002} "
-vim.g["lightline#lsp#indicator_infos"] ="\u{f129} "
-vim.g["lightline#lsp#indicator_warnings"] = "\u{f071} "
-vim.g["lightline#lsp#indicator_errors"] = "\u{f05e} "
-vim.g["lightline#lsp#indicator_ok"] = "\u{f00c}"
