d01649a5fe0a285f2aeb1fd65fb23c082f11992a
Daniel Duan <daniel@duan.ca>
2023-06-18 12:33:39 -0700

    [Alacritty] Change colors to gruvbox hard

--- config/alacritty.nix
+++ config/alacritty.nix
@@ -17,19 +17,19 @@
     font = {
       size = 14.0;
       normal = {
-        family = "SF Mono";
+        family = "SF Mono Nerd";
         style = "Regular";
       };
       bold = {
-        family = "SF Mono";
+        family = "SF Mono Nerd";
         style = "Bold";
       };
       italic = {
-        family = "SF Mono";
+        family = "SF Mono Nerd";
         style = "Italic";
       };
       bold_italic = {
-        family = "SF Mono";
+        family = "SF Mono Nerd";
         style = "Bold Italic";
       };
     };
@@ -43,39 +43,39 @@
     };
     cursor = {
       style.blinking = "Always";
-      blink_interval = 500;
+      blink_interval = 800;
     };
     colors = {
       # Default colors
       primary = {
-        background = "0x212121";
-        foreground = "0xdcdccc";
+        background = "0x1d2021";
+        foreground = "0xd4be98";
       };
 
       # Colors the cursor will use if `custom_cursor_colors` is true
 
       # Normal colors
       normal = {
-        black = "0x4d4d4d"; # 0
-        red = "0xfb2573"; # 1
-        green = "0x98e123"; # 2
-        yellow = "0xeedfb5"; # 3
-        blue = "0x45a9d0"; # 4
-        magenta = "0xd190c0"; # 5
-        cyan = "0x1070b0"; # 6
-        white = "0xdcdbcd"; # 7
+        black = "0x32302f"; # 0
+        red = "0xea6962"; # 1
+        green = "0xa9b665"; # 2
+        yellow = "0xd8a657"; # 3
+        blue = "0x7daea3"; # 4
+        magenta = "0xd3869b"; # 5
+        cyan = "0x89b482"; # 6
+        white = "0xd4be98"; # 7
       };
 
       # Bright colors
       bright = {
-        black = "0x768f81"; # 8
-        red = "0x999999"; # 9
-        green = "0xc2bfa2"; #10
-        yellow = "0xddcfa4"; #11
-        blue = "0x9dbdee"; #12
-        magenta = "0xe097d0"; #13
-        cyan = "0xa4dee2"; #14
-        white = "0xffffff"; #15
+        black = "0x32302f"; # 8
+        red = "0xea6962"; # 9
+        green = "0xa9b665"; #10
+        yellow = "0xd8a657"; #11
+        blue = "0x7daea3"; #12
+        magenta = "0xd3869b"; #13
+        cyan = "0x89b482"; #14
+        white = "0xd4be98"; #15
       };
     };
   };
