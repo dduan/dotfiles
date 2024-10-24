231ef8571dda0f712171acfa23d8c77ace2a8a42
Daniel Duan <daniel@duan.ca>
2024-10-24 10:51:46 -0700

    Add tig config

--- config/home/darwin.nix
+++ config/home/darwin.nix
@@ -1,8 +1,14 @@
 { config, pkgs, pkgs-unstable, ... }:
 import ./common.nix // {
-  home.file = {
-    "Library/Application Support/Rectangle/RectangleConfig.json".source = ../RectangleConfig.json;
+  home =
+    {
+      file = {
+        "Library/Application Support/Rectangle/RectangleConfig.json".source = ../RectangleConfig.json;
+        ".config/tig/config".source = ../tig_config_darwin;
+      };
+      packages = import ../packages/darwin.nix { inherit pkgs; };
+    };
+  programs = import ../programs/darwin.nix {
+    inherit pkgs pkgs-unstable;
   };
-  home.packages = import ../packages/darwin.nix { inherit pkgs; };
-  programs = import ../programs/darwin.nix { inherit pkgs pkgs-unstable; };
 }
--- config/home/linux.nix
+++ config/home/linux.nix
@@ -1,6 +1,11 @@
 { config, pkgs, pkgs-unstable, ... }:
 import ./common.nix // {
-  home.packages = import ../packages/linux.nix { inherit pkgs; };
+  home = {
+    packages = import ../packages/linux.nix { inherit pkgs; };
+    file = {
+      ".config/tig/config".source = ../tig_config_linux;
+    };
+  };
   programs = import ../programs/linux.nix { inherit pkgs pkgs-unstable; };
   xsession.windowManager = {
     i3 = (import ../../lib).callPackages pkgs ../i3.nix { };
