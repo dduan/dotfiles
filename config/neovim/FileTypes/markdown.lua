33a2114927e8be57dbfecb573336e77ab2c06a8b
Daniel Duan <daniel@duan.ca>
2026-08-28 11:44:17 -0700

    [Neovim] Fix markdown

--- config/neovim/FileTypes/markdown.lua
+++ config/neovim/FileTypes/markdown.lua
@@ -2,3 +2,11 @@
 vim.g.vim_markdown_folding_disabled = 1
 vim.g.vim_markdown_conceal_code_blocks = 0
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function(args)
+        vim.treesitter.stop(args.buf)
+        vim.bo[args.buf].syntax = "markdown"
+    end,
+})
+
